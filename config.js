76d4aeb213f3eeacc4bf2b00e314c10e32accda6
franjah <francotre_2010@hotmail.com>
2018-06-25 22:58:17 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         'OhrGfPSidCD3bnnIY252Z1fvU',
+  consumer_secret:      '5bAMm7vZf8znJd8LS8uJ16YYqTTw6iZLS7gJ6jGnsJFu0qM30T',
+  access_token:         '183811903-7x3XZteWOExDuMCXhGIPoimdRPzBWw8rECMiBmlT',
+  access_token_secret:  'nA9HLHS0FX1m3snCoh5JvE31L3Braqnvr0izkPyXpVLVZ'
+}
+
+module.exports = config;
---
+++ server.js
@@ -0,0 +1,79 @@
+var fs = require('fs'),
+    path = require('path'),
+    Twit = require('twit'),
+    config = require(path.join(__dirname, 'config.js'));
+
+var T = new Twit(config);
+
+var moveFile = (file, dir2)=>{
+  //include the fs, path modules
+  var fs = require('fs');
+  var path = require('path');
+
+  //gets file name and adds it to dir2
+  var f = path.basename(file);
+  var dest = path.resolve(dir2, f);
+
+  fs.rename(file, dest, (err)=>{
+    if(err) throw err;
+    else console.log('Successfully moved');
+  });
+};
+
+
+function random_from_array(images){
+	return images[Math.floor(Math.random() * images.length)];
+}
+
+function upload_random_image(images){
+  console.log('Opening an image...');
+  var image_path = path.join(__dirname, '/images/' + random_from_array(images)),
+      b64content = fs.readFileSync(image_path, { encoding: 'base64' });
+
+  console.log('Uploading an image...');
+
+  T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+    if (err){
+      console.log('ERROR:');
+      console.log(err);
+    }
+    else{
+      console.log('Image uploaded!');
+      console.log('Now tweeting it...');
+
+      T.post('statuses/update', {
+        media_ids: new Array(data.media_id_string)
+      },
+        function(err, data, response) {
+          if (err){
+            console.log('ERROR:');
+            console.log(err);
+          }
+          else{
+            console.log('Posted an image!');
+          }
+        }
+      );
+    }
+  });
+moveFile(image_path, './usados/'); //mueve la imagen de 'images' a 'usados'
+}
+
+fs.readdir(__dirname + '/images', function(err, files) {
+  if (err){
+    console.log(err);
+  }
+  else{
+    var images = [];
+    files.forEach(function(f) {
+      images.push(f);
+    });
+
+    setInterval(function(){
+      upload_random_image(images);
+    }, 600000);
+  }
+});
+
+
+
